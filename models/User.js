5629d1bfa76d4f17353b6f9525e4b485682d3596
luisnueda <nueda.luis@gmail.com>
2018-04-19 13:09:25 +0200

    iteration 1 done

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  status: {type: String, enum:["Pending Confirmation", "Active"], default: "Pending Confirmation" },
+  confirmationCode: {type: String, unique: true},
+  email: String
 }, {
   timestamps: {
     createdAt: 'created_at',
@@ -13,3 +16,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
+
