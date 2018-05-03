89549d865ad151566215c0e358178abfce6e7271
Franco Vargas <francovargas25@gmail.com>
2018-05-03 15:08:22 -0500

    got it to mail confirmation

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,25 @@ const express = require("express");
 const passport = require('passport');
 const authRoutes = express.Router();
 const User = require("../models/User");
+const nodemailer = require("nodemailer")
+
+let transporter = nodemailer.createTransport({
+  service:"Gmail",
+  auth:{
+    user:process.env.EMAIL_USER,
+    pass:process.env.EMAIL_PASSWORD
+  }
+})
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+
+authRoutes.get("/confirm/:confirmCode", (req,res,next) =>{
+  User.findOnedAndUpdate({confirmationCode: req.params.confirmCode},{ $set: {status:"Active"} })
+  .then(()=>res.redirect("/"))
+  .catch(e => console.log(e))
+});
 
 
 authRoutes.get("/login", (req, res, next) => {
@@ -27,6 +42,7 @@ authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const rol = req.body.role;
+  const email = req.body.email
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -40,18 +56,29 @@ authRoutes.post("/signup", (req, res, next) => {
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
+    const confirmationCode = bcrypt.hashSync(username,salt)
 
     const newUser = new User({
       username,
+      email,
       password: hashPass,
-      role:"teacher"
+      role:"teacher",
+      confirmationCode
     });
-
+    console.log(newUser)
     newUser.save((err) => {
       if (err) {
+        console.log(err);
         res.render("auth/signup", { message: "Something went wrong" });
       } else {
         res.redirect("/");
+        let message = {
+          from: "francovargas25@gmail.com",
+          to: email,
+          subject: "Tu codigo de confirmación",
+          html: `<a href="http://localhost:3000/auth/confirm/${confirmationCode}">Confirma ahora</a>`
+    };
+    transporter.sendMail(message)
       }
     });
   });
