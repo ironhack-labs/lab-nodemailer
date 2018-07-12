274ba1c2c9b51df723d219a6813f4f042d05ce4e
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-07-12 17:28:33 +0200

    done

---
+++ lab-nodemailer/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  status: {
+    "type": "string",
+    "enum": ["Pending Confirmation", "Active"]
+},
+  confirmationCode: String,
+  email: String,
+  // timestamps: {
+  //   createdAt: 'created_at',
+  //   updatedAt: 'updated_at'
+  // }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
