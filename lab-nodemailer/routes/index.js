f52fb13a4d4cb244bc674088e1a543be85623c8f
Ruben <rubenvilchesperez1985@hotmail.com>
2018-07-12 19:11:52 +0200

    Iteracion 1

---
+++ lab-nodemailer/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/send-email', (req, res, next) => {
+  let { email, subject, message } = req.body;
+  res.render('message', { email, subject, message })
+});
+
+module.exports = router;
+
+
+
