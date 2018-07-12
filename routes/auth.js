465e514cf8297683f9d9a24cd41b0c311481426d
Verónica Reina <veroreinah@gmail.com>
2018-07-12 16:01:00 +0200

    Iteration 2 - Signup Process

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
+const env = require('dotenv');
+env.config();
+env.config({path: './.env.private'});
+
 const express = require("express");
 const passport = require('passport');
 const authRoutes = express.Router();
 const User = require("../models/User");
+const { transporter } = require("../mailing/transporter");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -24,37 +29,58 @@ authRoutes.get("/signup", (req, res, next) => {
 });
 
 authRoutes.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const rol = req.body.role;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
-    return;
-  }
-
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
+  const { username, password, email } = req.body;
+  
+  var fieldsPromise = new Promise((resolve, reject) => {
+    if (username === "" || password === "" || email === "") {
+      reject(new Error("Indicate a username, email and password to sign up"));
+    } else {
+      resolve();
     }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      password: hashPass,
-      role:"teacher"
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        res.render("auth/signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("/");
-      }
-    });
   });
+
+  fieldsPromise
+    .then(() => {
+      return User.findOne({ username });
+    })
+    .then(user => {
+      if (user) {
+        throw new Error("The username already exists");
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const confirmationCode = encodeURIComponent(bcrypt.hashSync(username, salt));
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashPass,
+        confirmationCode
+      });
+
+      return newUser.save();
+    })
+    .then(user => {
+      res.redirect("/");
+
+      const message = `http://localhost:3000/auth/confirm/${user.confirmationCode}`;
+
+      transporter.sendMail({
+        from: process.env.GMAIL_USER,
+        to: user.email, 
+        subject: "Sign up confirmation", 
+        text: message,
+        html: `<b><a href="${message}">Confirm your account</a></b>`
+      })
+      .then(info => console.log(info))
+      .catch(error => console.log(error));
+    })
+    .catch(err => {
+      res.render("auth/signup", {
+        errorMessage: err.message
+      });
+    });
 });
 
 authRoutes.get("/logout", (req, res) => {
