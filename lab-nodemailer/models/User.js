dbf7ebe8ad1420285deb518520e051e4818d7518
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-12 14:04:29 +0200

    Iteration 1 DONE, technical difficulties with Ubuntu upon opening up Compass

--- lab-nodemailer/models/User.js
+++ lab-nodemailer/models/User.js
@@ -3,7 +3,12 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  status: { type: String, default: "Pending Confirmation" } ,
+  confirmationCode: {
+    enum: { type: String, unique: true }
+  },
+  emailAddress: String,
 }, {
   timestamps: {
     createdAt: 'created_at',
