3875ba3814caf756f0caddfbdbae91ad093af17f
Ricardo Rea <rickirea@gmail.com>
2018-07-19 22:06:34 -0500

    Done

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const passport = require('passport');
 const authRoutes = express.Router();
 const User = require("../models/User");
+const nodemailer = require("nodemailer");
 
 // Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const bcryptSalt = 10;
 
 
@@ -26,6 +27,8 @@ authRoutes.get("/signup", (req, res, next) => {
 authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email    = req.body.email;
+
   const rol = req.body.role;
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
@@ -40,20 +43,69 @@ authRoutes.post("/signup", (req, res, next) => {
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
+    const hashConfirmationCode = bcrypt.hashSync(username, salt);
 
     const newUser = new User({
       username,
       password: hashPass,
-      role:"teacher"
+      email,
+      confirmationCode: hashConfirmationCode
     });
 
     newUser.save((err) => {
       if (err) {
         res.render("auth/signup", { message: "Something went wrong" });
       } else {
+        
+        let transporter = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: 'rickirea@gmail.com',
+            pass: 'G00gl3G00gl3' 
+          }
+        });
+
+        transporter.sendMail({
+          from: '"My Awesome Project 👻" <rickirea@gamil.com>',
+          to: 'rickirea@gmail.com', 
+          subject: 'Please confirm your signup', 
+          text: 'Hi ' + username + ', Please confirm your signup',
+          html: '<b><a href="http://localhost:3000/auth/confirm/'+hashConfirmationCode+'">Clicka aqui para confirmar</></b>'
+        })
+        .then(info => console.log(info))
+        .catch(error => console.log(error))
+
         res.redirect("/");
       }
     });
+  });
+});
+
+authRoutes.get('/confirm/:confirmCode', (req,res, next) =>{
+  console.log("Entrando en confirmacion");
+  const confirmCode = req.params.confirmCode;
+  //console.log(confirmCode);
+
+  User.findOne({ "confirmationCode": confirmCode }, (err, user) => {
+    if (err || !user) 
+    {
+      console.log("The username doesn't exist");
+      res.render("auth/login", {
+        errorMessage: "The username doesn't exist"
+      });
+      return;
+    }
+    else
+    {
+      User.update({_id: user._id}, { $set: {status: 'Active'}})
+      .then((response) => {
+        console.log({response});
+        res.render('auth/confirmation',{user});
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+    }
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/User");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/profile/:userid",(req, res, next) =>{
+  const userid = req.params.userid;
+
+  User.findOne({ "_id": userid }, (err, user) => {
+    if (err || !user) 
+    {
+      console.log("The username doesn't exist");
+      res.render("auth/login", {
+        errorMessage: "The username doesn't exist"
+      });
+      return;
+    }
+    else
+    {
+      res.render("profile", {user});
+    }
+  });
+})
 
 module.exports = router;
