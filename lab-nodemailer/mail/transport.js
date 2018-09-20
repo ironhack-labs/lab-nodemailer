a98464798f3d918dfced31cb31f4ad74ea6a292a
cesararmada <cesar.armada@gmail.com>
2018-09-20 11:46:45 +0200

    email sent

---
+++ lab-nodemailer/mail/transport.js
@@ -0,0 +1,28 @@
+require('dotenv').config({path: '.private.env'});
+const nodemailer = require('nodemailer');
+
+if(!process.env.GMAIL_USER || ! process.env.GMAIL_PASSW ){
+  throw new Error("You have to configure mail credentials in .private.env file.");
+}
+
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSW 
+  }
+});
+
+
+const sendMail = (to, subject, message)=>{
+  return transporter.sendMail({
+    to, 
+    subject, 
+    html: `<b>${message}</b>`
+  })
+  .then(info => console.log(info))
+  .catch(error => console.log(error))
+}
+
+
+module.exports = sendMail;
---
+++ lab-nodemailer/routes/auth.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+const sendMail = require('../mail/transport');
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    const hashPassConfirmation = bcrypt.hashSync(username, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      email,
+      confirmationCode : hashPassConfirmation,
+      
+    });
+
+    newUser.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .then(()=>{
+        sendMail(email,"Nuevo Usuario", `http://localhost:3000/auth/confirm/${hashPassConfirmation}`)
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
