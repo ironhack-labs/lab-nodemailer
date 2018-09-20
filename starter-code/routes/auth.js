f3dcb07eacd737891a0e5f7b21b9793adb172b9f
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-09-20 15:51:27 +0200

    Problems with the {new:true}

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -1,7 +1,10 @@
+require('dotenv').config({path: '.private.env'})
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const sendEmail = require('../mails/sendMail')
+const ensureLogin = require('connect-ensure-login')
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -26,6 +29,10 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashUser = bcrypt.hashSync(username, salt);
+  const confirmationCode = hashUser;
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -42,18 +49,36 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email,
+      confirmationCode
     });
 
     newUser.save()
     .then(() => {
       res.redirect("/");
     })
+    .then(() => {
+      console.log(process.env.GMAIL_USER);
+      console.log(process.env.GMAIL_PW);
+      
+      sendEmail(newUser.email, 'ACTIVA TU CUENTA',`http://localhost:3000/auth/confirm/${confirmationCode}`)
+    })
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong" });
     })
   });
 });
+
+router.get('/confirm/:id', ensureLogin.ensureLoggedIn('auth/login'), (req,res,next) => {
+  let code = req.params.id;
+  User.findOneAndUpdate({confirmationCode: code}, { $set: { status: 'Active'} })
+  .then((a) => {
+      res.render('profile', { a })
+    })
+  .catch(e => console.log(e))
+})
+
 
 router.get("/logout", (req, res) => {
   req.logout();
