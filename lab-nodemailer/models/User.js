e250757ddca965ac6957cc9dc4f666e10e9e452d
agdwm <almudenaguerraswm@gmail.com>
2018-09-20 11:17:10 +0200

    Iteration 1 done - User Model

--- lab-nodemailer/models/User.js
+++ lab-nodemailer/models/User.js
@@ -3,7 +3,10 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  status: { type: String, enum:['Pending Confirmation', 'Active'], default: 'Pending Confirmation' },
+  confirmationCode: { unique: true },
+  email: mongoose.SchemaTypes.email
 }, {
   timestamps: {
     createdAt: 'created_at',
