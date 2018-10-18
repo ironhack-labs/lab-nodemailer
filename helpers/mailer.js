ba0142b8aa70095759f89be7ab0c4efe85c84e5b
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-10-17 23:30:45 -0500

    Avances Pair Porgramming

---
+++ helpers/mailer.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+const generateHtml = (filename, options={}) => {
+    const html = hbs.compile(fs.readFileSync((__dirname, `./views/mail/${filename}.hbs`),'utf8'));
+    return html(options);
+};
+
+exports.send = (options) => {
+    const html = generateHtml(options.filename,options);
+    const mailOptions = {
+        from    : "Pair Programming of Albert and Xime",
+        to      : options.email,
+        subject : options.subject,
+        text    : options.message,
+        html
+    };
+    return transporter.sendMail(mailOptions);
+};
