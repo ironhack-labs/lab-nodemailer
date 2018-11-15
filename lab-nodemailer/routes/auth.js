d187d9fc74b45a62001f03cd30bf77daf84c206d
Sarah Connor <sarahchamorro@gmail.com>
2018-11-15 08:31:56 -0200

    add confirmation view

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
@@ -8,24 +8,28 @@ const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 function codeGen() {
-  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let token = '';
-for (let i = 0; i < 25; i++) {
-    token += characters[Math.floor(Math.random() * characters.length )];
-}
-return token;
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let token = "";
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
 }
 
 router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -56,18 +60,69 @@ router.post("/signup", (req, res, next) => {
       password: hashPass,
       email,
       status,
-      confirmationCode,
+      confirmationCode
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(() => {
+        //send email
+        let message = `http://localhost:3000/auth/confirm/${newUser.confirmationCode}`;
+        let subject = "Confirm your signup";
+        let transporter = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "sarahchamorro02@gmail.com",
+            pass: "Mechakucha1!"
+          }
+        });
+        transporter
+          .sendMail({
+            from: '"Ironhack Confirmation Email 👻" <myawesome@project.com>',
+            to: newUser.email,
+            subject: subject,
+            text: message,
+            html: `<b><a>${message}</a></b>`
+          })
+          .then(info =>
+            res.render("message", { email, subject, message, info })
+          )
+          .catch(error => console.log(error));
+        res.redirect("/");
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
   });
 });
+
+router.get("/auth/confirm/:confirmationCode", (req, res, next) => {
+  res.render("confirm");
+});
+
+/*
+router.post('/signup/auth', (req, res, next) => {
+  let message = `http://localhost:3000/auth/confirm/${confirmationCode}`;
+  let subject = 'Confirm your signup';
+  const email = req.body.password;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'sarahchamorro02@gmail.com',
+      pass: 'Mechakucha1!'
+    }
+  });
+  transporter.sendMail({
+    from: '"Ironhack Confirmation Email 👻" <myawesome@project.com>',
+    to: email, 
+    subject: subject, 
+    text: message,
+    html: `<b>${message}</b>`
+  })
+  .then(info => res.render('message', {email, subject, message, info}))
+  .catch(error => console.log(error));
+});
+*/
 
 router.get("/logout", (req, res) => {
   req.logout();
