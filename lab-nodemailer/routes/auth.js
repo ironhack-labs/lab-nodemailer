57e0cb0bc225b176a8eec30e19184ddb097e5b2d
Sarah Connor <sarahchamorro@gmail.com>
2018-11-15 13:08:38 -0200

    istall nodemailler and require it | google credentials not working

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -2,10 +2,17 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
-
-// Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: "sarahchamorro02@gmail.com",
+    pass: "Mechakucha1!"
+  }
+});
 
 function codeGen() {
   const characters =
@@ -69,29 +76,33 @@ router.post("/signup", (req, res, next) => {
         //send email
         let message = `http://localhost:3000/auth/confirm/${newUser.confirmationCode}`;
         let subject = "Confirm your signup";
-        let transporter = nodemailer.createTransport({
-          service: "Gmail",
-          auth: {
-            user: "sarahchamorro02@gmail.com",
-            pass: "Mechakucha1!"
-          }
+        transporter.sendMail({
+          from: "Miguel - Tarea que hice solo :(",
+          to: newUser.email,
+          subject: "Confirmar cuenta",
+          html: 
+  `<html>
+  <head></head>
+  <body style="background-color: #2B2F30">
+    <div style="font-family: 'Helvetica Neue', Helvetica;    text-align: center;    padding: 5px; ;"><img src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/4017/s300/logo-ironhack-blue.png" alt="Ironhack" style="padding-top: 15px;"/>
+      <p></p>
+      <div style="width: 80%;      max-width: 800px;      font-weight: 300;      margin: 0 auto;      background-color: white;      border-radius: 15px;      padding: 15px;      padding-bottom: 15px;">
+        <h1 style="font-size:24px; font-weight: 100;">Let's get started with this test, ${newUser.username}</h1>
+        <p style="font-size:18px; line-height: 1.5;">Confirm your email now to activate your account. If you've forgotten your password or username, you can create a new account or access Mongo and delete it yourself, lol.</p><a href="http://localhost:3000/auth/confirm/${newUser.confirmationCode}" style="padding: 15px;        font-family: 'Helvetica Neue', Helvetica;        text-size: 18px;        color: white;        background-color: #52C3FB;        border: 0;        border-radius: 5px;        margin: 10px;        display: block;        max-width: 200px;        margin: auto;        text-decoration: none;">Confirm email address</a>
+        <h2 style="  font-weight: 200;        color: #52C3FB;">Lorem Ipsum</h2>
+        <p font-size:16px; style="line-height: 1.5;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum id nunc quis tempor. Duis sed libero ornare felis bibendum dapibus. Suspendisse condimentum varius felis a rutrum. Aliquam posuere feugiat arcu, non gravida ipsum lacinia sit amet. Morbi euismod nisi a enim placerat, non venenatis diam pretium.</p>
+      </div>
+    </div>
+    <div style=" margin: 0 auto;    padding: 20px;    display: block;    width: 200px;    text-align: center;"><img src="https://lomotif-prod.s3.amazonaws.com/stickers/Email/Facebook.png" alt="Facebook"/><img src="https://lomotif-prod.s3.amazonaws.com/stickers/Email/Twitter.png" alt="Twitter"/><img src="https://lomotif-prod.s3.amazonaws.com/stickers/Email/IG.png" alt="Instagram"/></div>
+  </body>
+  </html>`
+  
         });
-        transporter
-          .sendMail({
-            from: '"Ironhack Confirmation Email 👻" <myawesome@project.com>',
-            to: newUser.email,
-            subject: subject,
-            text: message,
-            html: `<b><a>${message}</a></b>`
-          })
-          .then(info =>
-            res.render("message", { email, subject, message, info })
-          )
-          .catch(error => console.log(error));
-        res.redirect("/");
+  
+  res.redirect(`/`);
       })
       .catch(err => {
-        res.render("auth/signup", { message: "Something went wrong" });
+        res.render("auth/signup", { message: err });
       });
   });
 });
