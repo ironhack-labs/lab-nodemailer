f8be86278e695214dac4e89c78c4ed6b87b2d4fa
PomPom248 <sofiroggero@gmail.com>
2018-11-22 00:08:49 +0100

    iteration 2

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -1,8 +1,13 @@
+// require('dotenv').config();
+
+const nodemailer = require("nodemailer")
+
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
 
+const transporter = require("../mail/transporter")
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
@@ -26,6 +31,8 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -40,24 +47,43 @@ router.post("/signup", (req, res, next) => {
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
+    const hashCode = bcrypt.hashSync(username, salt)
+
+
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email,
+      confirmationCode: hashCode
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
-  });
-});
+    const sendMail = (from, to, subject, text) => {
+      return transporter.sendMail({
+        from: 'Myself',
+        to: email,
+        subject: 'Registration Confirmed',
+        text: `Confirm registration here: http://localhost:3000/auth/confirm/${hashCode}`
+      })
+        .then(info => console.log(info))
+        .catch(error => console.log(error));
+    }
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+    newUser.save()
+      .then((user) => {
+        sendMail(user).then(() => {
+          console.log("mensaje enviado")
+          res.redirect("/");
+        })
+          .catch(err => {
+            res.render("auth/signup", { message: "Something went wrong" });
+          })
+      });
+  });
+
+  router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
