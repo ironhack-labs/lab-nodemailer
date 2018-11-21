e090b2b734bdaf31d087a9f57ccc0b645d125081
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-11-21 21:39:29 +0100

    iteration 2

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const transporter = require('../mail/transporter');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -26,6 +27,8 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -40,10 +43,20 @@ router.post("/signup", (req, res, next) => {
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let token = '';
+    for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length )];
+}
+
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email: email,
+      confirmationCode:token,
     });
+
+    console.log(email)
 
     newUser.save()
     .then(() => {
@@ -52,6 +65,19 @@ router.post("/signup", (req, res, next) => {
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong" });
     })
+
+    transporter.sendMail({
+      from: '"My Awesome Project 👻" <diegoesunperson@gmail.com>',
+      to: email, 
+      subject: 'Awesome Subject',
+      text: 'Awesome Subject',
+      html: `http://localhost:3000/auth/confirm/${token}`,
+    })
+    .then(info => res.render('message', {email, subject, message, info}))
+    .catch(error => console.log(error));
+
+
+
   });
 });
 
@@ -59,5 +85,22 @@ router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
