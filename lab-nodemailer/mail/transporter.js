ee68b04f9c47b10052bb13ad9050b581e4c6a300
PomPom248 <sofiroggero@gmail.com>
2018-11-22 09:36:54 +0100

    iteration 2

--- lab-nodemailer/mail/transporter.js
+++ lab-nodemailer/mail/transporter.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'Gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
