f2477b86fc315bbc663e741c1fc6f14dbd157cae
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-22 20:37:24 +0100

    Iteration 2

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require('passport');
+const transporter = require('../mail/transporter');
 const router = express.Router();
 const User = require("../models/User");
 
@@ -24,8 +25,15 @@ router.get("/signup", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+  const confirmationCode = token;
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -42,16 +50,26 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email,
+      confirmationCode
     });
 
     newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+      .then(() => {
+        transporter.sendMail({
+          from: '"My Awesome Project 👻" <myawesome@project.com>',
+          to: email,
+          subject: 'Awesome Subject',
+          text: 'Awesome Message',
+          html: `<a href="http://localhost:3000/auth/confirm/${confirmationCode}">WEB TO WAPA<a>`,
+        })
+          .then(() => res.redirect("/"))
+          .catch(err => console.log(err));
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      })
   });
 });
 
