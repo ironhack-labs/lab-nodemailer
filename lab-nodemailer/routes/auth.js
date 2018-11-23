cb37a7d9446e4d483bdf22ee526d966451a7c513
cgandiaf <c.gandia.f@gmail.com>
2018-11-23 07:26:40 +0100

    Iteration 4

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -2,18 +2,18 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const transporter = require('../mail/transporter');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login", { "message": req.flash("error") });
 });
 
 router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
+  successRedirect: "/auth/profile/:id",
   failureRedirect: "/auth/login",
   failureFlash: true,
   passReqToCallback: true
@@ -26,7 +26,11 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  if (username === "" || password === "") {
+  const email = req.body.email;
+  const status = "Pending Confirmation";
+  const confirmationCode = setConfirmationCode();
+
+  if (username === "" || password === "" || email === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
   }
@@ -42,22 +46,61 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
-    });
+      password: hashPass,
+      status,
+      confirmationCode,
+      email
+    })
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(() => {
+        //console()
+        transporter.sendMail({
+          from: '"Nodemailer Proyect" <ironhackpruebas@gmail.com>',
+          to: email,
+          subject: 'Awesome Subject',
+          text: 'Awesome Message',
+          html: `<a href="http://localhost:3000/auth/confirm/${confirmationCode}">VALIDACION<a>`,
+        })
+        res.redirect("/")
+        .catch(err => console.log(err));
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      })
   });
-});
+})
 
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+
+router.get("/confirm/:confirmationCode", (req, res, next) => {
+  let code = {
+    confirmationCode: req.params.confirmationCode
+  }
+  User.findOneAndUpdate(code, {$set: {status: 'Active'} })
+  .then((user) => res.render("auth/confirm"))
+  .catch(err => console.log(err));
+});
+
+router.get("/profile/:id", (req, res, next) => {
+  User.findById(req.session.passport.user)
+  .then(user =>{
+    res.render("auth/profile", {user});
+  })
+})
+
+let setConfirmationCode = () => {
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let token = "";
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token
+};
 
 module.exports = router;
