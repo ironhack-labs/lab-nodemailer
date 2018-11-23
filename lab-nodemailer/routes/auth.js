24c3a68387148c4a23f8c43a8e633861c739d2c4
David <davidmunozvi@icloud.com>
2018-11-23 09:39:07 +0100

    Llegan los correos

--- lab-nodemailer/routes/auth.js
+++ lab-nodemailer/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const passport = require('passport');
+const transporter = require('../mail/transporter');
 const router = express.Router();
 const User = require("../models/User");
-
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+
 
 
 router.get("/login", (req, res, next) => {
@@ -26,8 +27,14 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length )];
+}
   if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+    res.render("auth/signup", { message: "Indicate username, password and email" });
     return;
   }
 
@@ -42,12 +49,22 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email,
+      confirmationCode: token
     });
 
-    newUser.save()
+    newUser.save() 
     .then(() => {
-      res.redirect("/");
+      transporter.sendMail({
+        from: "user <myawesome@project.com>",
+        to: email,
+        subject: 'Registration Confirmed',
+        text: `Confirm registration here: http://localhost:3000/auth/confirm/${token}`
+      })
+    })
+    .then(() => {
+      res.render("message", { email, subject, message, info })
     })
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong" });
