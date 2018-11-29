d1f4ade7e64792e26beea7cdd09a05097fc9a269
Gerardo <gera@MacBook-de-Gerardo.local>
2018-11-28 21:47:24 -0600

    No entendí bien como poner el nodemail :(

---
+++ email/routes/auth.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const {username, email, password} = req.body
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let token = '';
+    for (let i = 0; i < 25; i++) {
+        token += characters[Math.floor(Math.random() * characters.length )];
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass, 
+      email,
+      confirmationCode: token
+    });
+
+    console.log(newUser)
+
+    newUser.save()
+    .then((user) => {
+      console.log(user)
+      nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+                 user: process.env.USER,
+                 pass: process.env.PASS
+             }
+         })
+    
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Gera ❤️" <gerard.os.jr@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: 'Hello 😎 ✔', // Subject line
+            html: `Hola!, Este es un mail de spam, formatea ahora 👀`
+        };
+    
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    });
+
+
+
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
