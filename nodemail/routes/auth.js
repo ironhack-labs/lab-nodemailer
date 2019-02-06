940f73d54ae7ead5b0cd2ec832e637a67468baf7
Mr.X <seniorito.x@icloud.com>
2019-02-07 00:02:34 +0100

    First commit Iteration 1 & 2 🤘

---
+++ nodemail/routes/auth.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email    = req.body.email;
+  const confirmationCode = `${Math.random()}`;
+
+  if (username === "" || password === "" || email === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      email,
+      confirmationCode,
+    });
+
+    newUser.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+//AÑADIR AQUÍ MÉTODO PARA MANDAR EL MAIL CON UN ENLACE A LA PÁGINA DEBEMOS INSERTAR EN LA URL QUE LE MANDAMOS EL CONFIRMATION CODE Y QUE AL HACER CLICK LE LLEVE A 
+  .then()
+  .catch()
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
