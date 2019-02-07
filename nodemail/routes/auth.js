daead770d03f28852320d6e949b63fb771573a2d
Mr.X <seniorito.x@icloud.com>
2019-02-07 10:18:02 +0100

    Semi Done 👾

--- nodemail/routes/auth.js
+++ nodemail/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const nodemailer = require('nodemailer')
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -50,17 +51,45 @@ router.post("/signup", (req, res, next) => {
       confirmationCode,
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+  newUser
+      .save()
+      .then(user => {
+        let transporter = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "pruebanodemailer0119@gmail.com",
+            pass: "Prueba-nodemailer-0119"
+          }
+        });
+
+         transporter
+          .sendMail({
+            from: '"Confirm User" <myawesome@project.com>',
+            to: user.email,
+            subject: "Confirm your Sign Up 🤘",
+            html: `<b>http://localhost:3000/auth/confirm/${
+              user.confirmationCode
+            }</b>`
+          })
+          .then(info => console.log('email sent OK'))
+          .catch(error => console.log(error));
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
   });
-//AÑADIR AQUÍ MÉTODO PARA MANDAR EL MAIL CON UN ENLACE A LA PÁGINA DEBEMOS INSERTAR EN LA URL QUE LE MANDAMOS EL CONFIRMATION CODE Y QUE AL HACER CLICK LE LLEVE A 
-  .then()
-  .catch()
+});
+
+router.get("/confirm/:code", (req, res, next) => {
+  User.findOne({ confirmationCode: req.params.code }).then(user => {
+    if (user !== null) {
+      User.findByIdAndUpdate(user._id, { status: "Active" }).then(user =>
+        res.redirect("/confirmation")
+      );
+      return;
+    }
+    res.redirect("/auth/signup");
+  });
 });
 
 router.get("/logout", (req, res) => {
