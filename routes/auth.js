7487dcbde4d0d46cbacf1916aee851c0a1e33f08
Raul Contreras <raulc98@gmail.com>
2019-04-17 21:49:47 +0200

    Finished iteration 2

--- routes/auth.js
+++ routes/auth.js
@@ -26,15 +26,22 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  const email = req.body.email;
+  if (username === "" || password === "" || email === "") {
+    res.render("auth/signup", { message: "Indicate username, password and email" });
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
+  User.findOne({ $or: [{username}, {password}] }, (err, user) => {
     if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+      res.render("auth/signup", { message: "The username or email already exists" });
       return;
+    }
+
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let confirmationCode = '';
+    for (let i = 0; i < 25; i++) {
+        confirmationCode += characters[Math.floor(Math.random() * characters.length )];
     }
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
@@ -42,11 +49,31 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
+      email, 
+      confirmationCode,
       password: hashPass
     });
 
     newUser.save()
     .then(() => {
+      let subject = 'Please, confirm your email';
+      let message = `To activate your account, please follow the next link: <a href="http://localhost:3000/auth/confirm/${confirmationCode}">http://localhost:3000/auth/confirm/${confirmationCode}</a>`;
+      let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'madwebmar19@gmail.com',
+          pass: 'iron2019'
+        }
+      });
+      transporter.sendMail({
+        from: '"My Awesome Project 👻" <myawesome@project.com>',
+        to: email, 
+        subject: subject, 
+        text: message,
+        html: message
+      })
+      .then(info => res.render('message', {email, subject, message, info}))
+      .catch(error => console.log(error));
       res.redirect("/");
     })
     .catch(err => {
@@ -58,6 +85,10 @@ router.post("/signup", (req, res, next) => {
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
+});
+
+router.get('/confirm/:confirmationCode', (req, res, next) => {
+  res.json({hola:'hola'});
 });
 
 module.exports = router;
