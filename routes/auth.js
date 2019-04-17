fb6ccb497f776637aa5f5fbaece606b66f7d89ff
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-17 13:38:57 +0200

    Iteration 2

---
+++ routes/auth.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+const nodemailer = require('nodemailer');
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const confirmationCode = Math.random().toString(36).substring(2, 15) 
+                         + Math.random().toString(36).substring(2, 15);
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      email,
+      confirmationCode
+    });
+
+    newUser.save()
+    .then(() => {
+      let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.MAILACCOUNT,
+          pass: process.env.PASS
+        }
+      });
+      transporter.sendMail({
+        from: '"Proof email 👻"',
+        to: email, 
+        subject: 'Validate your account', 
+        text: `http://localhost:3000/auth/confirm/${confirmationCode}`,
+        html: `<a href='http://localhost:3000/auth/confirm/${confirmationCode}'>Validate account</a>`
+      })
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
