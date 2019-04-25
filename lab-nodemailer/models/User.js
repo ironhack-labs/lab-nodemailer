5c8c67886e007b351b2c28037d20ee80f956b092
AhuiVargas <ahuijr@gmail.com>
2019-04-25 00:15:28 -0500

    first commit

---
+++ lab-nodemailer/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  status: {
+    type: String,
+    enum: ["Active", "Pending Confirmation"],
+    default: "Pending Confirmation"
+  },
+  confirmationCode:{
+    type:String,
+    require:true
+  },
+  email:{
+    type:String,
+    require:true
+  }
+}, {
+  versionKey: false,
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
