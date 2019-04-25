7e1ca7162fc61834cf6f730ed710b0497cba28bf
pipepico <fpr29.10@gmail.com>
2019-04-25 01:15:26 -0500

    adding middleware

--- lab-nodemailer/helpers/nodemailer.js
+++ lab-nodemailer/helpers/nodemailer.js
@@ -7,3 +7,20 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASS
   }
 })
+
+exports.sendConfCode = (email,confCode) => {
+  transporter.sendMail({
+    from: '"Agemeddi" <sign@agemeddi.com>',
+    to: email,
+    subject: "Welcome to Agemeddi! Confirm Your Email",
+    html:`
+    <h3>You're on your way! 😆</h3>
+    <p>Let's confirm your email address.</p>
+    <p>By clicking on the following link, you are confirming your email address.</p>
+    <p><a href="http://localhost:3000/auth/confirm/${confCode}">Confirm mail</a></p>
+    `
+  }).then((response) => {
+    console.log(response);
+  })
+  .catch( err => console.log(err))
+}
--- lab-nodemailer/routes/index.js
+++ lab-nodemailer/routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// router.get('/signup', (req, res, next) => {
+//   res.render('sign')
+// })
+
+// router.get('/login', (req, res, next) => {
+//   res.render('login')
+// })
+
+// router.post('/signup', (req, res, next) => {
+//   User.register({ ...req.body }, req.body.password)
+//     .then(user => {
+//       res.redirect('/login')
+//     })
+//     .catch(err => {
+//       res.send(err)
+//     })
+// })
+
+// router.post('/login', (req, res, next) => {
+//   passport.authenticate('local', (err, user, info) => {
+//     if (err) return next(err)
+//     if (!user) return res.redirect('/login')
+//     req.logIn(user, err => {
+//       if (err) return next(err)
+//       req.app.locals.loggedUser = req.user
+//       res.redirect('/profile')
+//     })
+//   })(req, res, next)
+// })
+
+// router.get('/profile', isLogged, /** */(req, res, next) => res.render('profile'))
 
 module.exports = router;
