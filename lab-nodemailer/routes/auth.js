a93c5920904a9dcd353c22dd87d28e0ba5760c6f
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-07-03 17:55:28 +0200

    done

---
+++ lab-nodemailer/routes/auth.js
@@ -0,0 +1,109 @@
+require('dotenv').config();
+
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+const nodemailer = require('nodemailer')
+
+// Bcrypt to encrypt passwords
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: `${process.env.USER_NAME}`,
+    pass: `${process.env.PASS}` 
+  }
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/auth/profile",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email
+
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < 25; i++) {
+      token += characters[Math.floor(Math.random() * characters.length )];
+  }
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      email: email,
+      confirmationCode: token
+    });
+
+    newUser.save()
+    .then(() => {
+      transporter.sendMail({
+        from: '"My Awesome Project 👻" <vanesa.pruebag@gmail.com>',
+        to: email, 
+        subject: 'Confirmacion de correo Ironhack', 
+        text: 'Esquilame',
+        html: `<b>Awesome Message</b> <a href="http://localhost:3000/auth/confirm/${token}">Equilame</a>`
+      })
+      .then(info => console.log(info))
+      .catch(error => console.log(error))
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get("/profile", (req, res) => {
+  res.render("auth/profile", { "message": req.flash("error") });
+});
+
+
+router.get("/confirm/:token", (req, res) => {
+  User
+  .findOneAndUpdate({confirmationCode:req.params.token}, {$set:{status:"Active"}},{new:true})
+  .then((user)=>{
+    res.redirect("/auth/login");
+    console.log ("User activated")
+  }).catch((err)=>{
+    console.log(err)
+  })
+});
+
+module.exports = router;
