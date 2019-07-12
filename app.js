f6d202b9cd08c179b390f0aec45daa197220910b
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-07-12 11:02:29 -0300

    fixed db reference

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/lab-nodemailer', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
