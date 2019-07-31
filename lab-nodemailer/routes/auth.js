de8bd7ff723d59c6a7583e0970697742b74ac152
Dazzsen <and24_21@msn.com>
2019-07-31 18:54:53 +0200

    done

---
+++ lab-nodemailer/routes/auth.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+const nodemailer = require('nodemailer')
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const correo = req.body.email;
+  if (username === "" || password === "" || correo === "") {
+    res.render("auth/signup", { message: "Rellena todos los campos" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+
+    User.findOne({ correo }, "email", (err, cor) => {
+      if (cor !== null) {
+        res.render("auth/signup", { message: "The email already exists" });
+        return;
+      }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+      const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let token = '';
+      for (let i = 0; i < 25; i++) {
+       token += characters[Math.floor(Math.random() * characters.length )];
+      }
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      confirmationCode: token,
+      email: correo
+    });
+
+let message=`<a href="http://localhost:3000/auth/confirm/${token}">Pincha aquí</a>`
+
+let subject="Virus"
+
+
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.MAIL,
+        pass: process.env.PASS
+      }
+    });
+  
+  
+  
+    transporter.sendMail({
+      from: '"Ironhacker Email 👻" <myawesome@project.com>',
+      to: correo,
+      subject: subject,
+      text: message,
+      html: `<b>${message}</b>`
+    })
+      .then(info => res.render('message', { correo, subject, message, info }))
+      .catch(error => console.log(error));
+
+
+    
+
+    newUser.save()
+    .then(() => {
+      res.redirect("/");
+      
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+})
+module.exports = router;
