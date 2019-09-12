c045ef70a159ed8f4d89f39800cf26692e321ac0
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-12 09:21:24 -0500

    done

---
+++ lab-nodemailer/models/User.js
@@ -0,0 +1,36 @@
+const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  status: {
+    type: String,
+    enum: ["PENDING CONFIRMATION", "ACTIVE"],
+    default: "PENDING CONFIRMATION",
+  },
+  confirmationCode: String,
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+
+});
+
+userSchema.plugin(passportLocalMongoose, {usernameField:'email'})
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+
+
