97d9284e8215383ac6cb66b5d1f64c1f3d403f45
“Natalia <natgarea@gmail.com>
2019-09-18 15:18:03 +0200

    iteration 1: user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  status: { enum: ['Pending Confirmation', 'Active'] },
+  confirmationCode: { type: String, unique: true },
+  email: { type: String, required: true, unique: true }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
