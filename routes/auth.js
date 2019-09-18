ec396aaa12f1bb1d319f5033ef309471f74998dc
Ester-Martinez <martinez.ester@gmail.com>
2019-09-18 18:25:34 +0200

    iteración 3

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,35 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
-const crypto = require('crypto');
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: `${process.env.EMAIL_ADDRESS}`,
+    pass: `${process.env.EMAIL_PASSWORD}`
+  }
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -29,12 +40,10 @@ router.post("/signup", (req, res, next) => {
   const password = req.body.password;
   const email = req.body.email;
 
-
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
   }
-
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
@@ -44,7 +53,7 @@ router.post("/signup", (req, res, next) => {
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
-    const confirmationCode = crypto.randomBytes(20).toString('hex'); 
+    const confirmationCode = crypto.randomBytes(20).toString("hex");
 
     const newUser = new User({
       username,
@@ -53,14 +62,30 @@ router.post("/signup", (req, res, next) => {
       confirmationCode
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(newUser => {
+        transporter
+          .sendMail({
+            from: '"My Awesome Project 👻" <myawesome@project.com>',
+            to: `${newUser.email}`,
+            subject: `${newUser.username}, welcome to My Awsome Project`,
+            text: "Awesome Message",
+            html: `<b><a href="http://localhost:3000/auth/confirm/${newUser.confirmationCode}">Click here to validate your account</a></b>`
+          })
+          .then(info => console.log(info))
+          .catch(error => console.log(error));
+        res.redirect("/");
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
   });
+});
+
+router.post("/send-email", (req, res, next) => {
+  let { email, subject, message } = req.body;
+  res.render("message", { email, subject, message });
 });
 
 router.get("/logout", (req, res) => {
