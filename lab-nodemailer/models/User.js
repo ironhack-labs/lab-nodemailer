72980d70258be2ae6dfa7271e2112a8ccb125684
María Simó <maria@coponstudio.es>
2019-11-20 20:39:19 +0100

    It 1

---
+++ lab-nodemailer/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  status : {
+    type: String,
+    enum : [
+      "Pending confirmation",
+      "Active"
+    ],
+    default : "Pending confirmation"
+  },
+  confirmationCode : {
+    type : String,
+    unique : true,
+  },
+  email : String,
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
