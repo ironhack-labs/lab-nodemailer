1c5c5808d86aec0c2a3615bbb5cbde1a98b04f35
mfernandezfreire <mfernandezfreire@outlook.com>
2020-02-13 00:02:18 +0100

    Aneurisma

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  status: {type: String, enum:["Pending Confirmation", "Active"], default: "Pending confirmation"},
+  confirmationCode: {type: String, unique: true} ,
+  email: {type: String, unique: true}
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
