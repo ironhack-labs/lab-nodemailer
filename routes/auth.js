103a510ea1a1f951f87acea4d267a882270b45a4
mfernandezfreire <mfernandezfreire@outlook.com>
2020-02-13 09:32:46 +0100

    Aneurisma2

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,20 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const nodemailer = require("nodemailer")
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+//transporter
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'pruebadeiron@gmail.com',
+    pass: 'ironhack'
+  }
+});
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login", {
@@ -29,11 +38,6 @@ router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
-  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let token = '';
-  for (let i = 0; i < 25; i++) {
-    token += characters[Math.floor(Math.random() * characters.length)];
-  }
   if (username === "" || password === "" || email === "") {
     res.render("auth/signup", {
       message: "Indicate username and password"
@@ -43,57 +47,47 @@ router.post("/signup", (req, res, next) => {
 
 
   User.findOne({
-    username
-  }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", {
-        message: "The username already exists"
-      });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username: username,
-      password: hashPass,
-      status: "Pending Confirmation",
-      confirmationCode: token,
-      email: email
-    });
-    console.log(newUser)
-
-
-    newUser.save()
-      .then((newUser) => {
-        let {
-          username,
-          email,
-          confirmationCode,
-        } = newUser;
-        let transporter = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'pruebadeiron@gmail.com',
-            pass: 'ironhack'
-          }
-        });
-        transporter.sendMail({
-            from: '"My Awesome Project " <pruebadeiron@gmail.com>',
-            to: email,
-            subject: 'Codigo de confirmación',
-            text: `Hola ${username} te enviamos tu código de confirmación`,
-            html: `<b>Hola!! Tu código de confirmación es${confirmationCode}</b>`
-          }).then(res.redirect('/'))
-          .catch(error => console.log(error));
-      })
-      .catch(err => {
+      username
+    }, "username", (err, user) => {
+      if (user !== null) {
         res.render("auth/signup", {
-          message: "Something went wrong"
+          message: "The username already exists"
         });
-      })
-  });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let token = '';
+      for (let i = 0; i < 25; i++) {
+        token += characters[Math.floor(Math.random() * characters.length)];
+      }
+      const newUser = new User({
+        username: username,
+        password: hashPass,
+        status: "Pending Confirmation",
+        confirmationCode: token,
+        email: email
+      });
+      transporter.sendMail({
+          from: '"My Awesome Project " <pruebadeiron@gmail.com>',
+          to: `${
+            newUser.email
+          }`,
+          subject: 'Codigo de confirmación',
+          text: `Hola ${newUser.username} te enviamos tu código de confirmación`,
+          html: `<b>Hola!! Tu código de confirmación es ${newUser.confirmationCode}</b>`
+        })
+        newUser.save()
+        .then(res.redirect('/'))
+        .catch(error => console.log(error));
+    })
+    .catch(err => {
+      res.render("auth/signup", {
+        message: "Something went wrong"
+      });
+    })
 });
 
 router.get("/logout", (req, res) => {
