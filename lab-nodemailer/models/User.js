443a9e52660e6301a34c0348cba505774e76dadb
Kevin Medina <kmedinakm@hotmail.com>
2020-03-05 19:24:15 -0500

    iteration 1 complete

---
+++ lab-nodemailer/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  status: {
+    type: String,
+    enum: ["Pending Confirmation", "Active"],
+    default: "Pending Confirmation",
+  },
+  confirmationCode: {
+    type: Number,
+    unique: true
+  },
+  email: {
+    type: String
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
