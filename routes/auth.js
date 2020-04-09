b64a7984077f62fe1cc27cb4e8efaddc949f36a6
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-09 19:00:10 -0300

    Configure nodemailer and test with mailtrap

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,24 @@ const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
 const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+
+//Template email import
+const template = require('../template/template');
+
+//Email transporter NODEMAILER
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3df9b8f6f5538a",
+    pass: "f88b3c0f8c97b8"
+  }
+});
 
 
 router.get("/login", (req, res, next) => {
@@ -56,7 +70,15 @@ router.post("/signup", (req, res, next) => {
     });
 
     newUser.save()
+
     .then(() => {
+      transport.sendMail({
+        from: "'Email confirmation' <example@gmail.com>",
+        to: email,
+        subject:"Confirm your email",
+        text:"Click on the link to confirm your register",
+        html:`<a href="http://localhost:3000/auth/confirm/${token}">Click here</a>`
+      })
       res.redirect("/");
     })
     .catch(err => {
@@ -65,9 +87,26 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
+router.get('/confirm/:token', (req, res) => {
+  let { token } = req.params;
+
+  User.findOneAndUpdate({ confirmationCode: token }, { status: 'Active'})
+  .then( response => {
+    console.log(response);
+    res.redirect('/');
+  })
+  .catch( error => {
+    console.log(error);
+    res.redirect('/signup');
+  })
+})
+
+
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+
+
 
 module.exports = router;
