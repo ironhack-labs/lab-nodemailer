f8b7e785a135680d22db121fdd2a3852d6c6a7b3
martaguirre91 <marta@FONETIC-MSI93.fonetic.local>
2020-07-19 19:55:02 +0200

    sending email working

--- models/User.js
+++ models/User.js
@@ -1,14 +1,42 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
+
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+const generateToken = () => {
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let token = '';
+    for (let i = 0; i < 25; i++) {
+        token += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return token
+}
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+    username: String,
+    password: String,
+    status: {
+        type: String,
+        enum: ['Pending Confirmation', 'Active'],
+    },
+    confirmationCode: {
+        type: String,
+        unique: true,
+        default: generateToken
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [EMAIL_PATTERN, "Email is invalid"],
+    }
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,23 @@
+require('dotenv').config()
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const nodemailer = require('nodemailer');
+const user = process.env.EMAIL;
+const pass = process.env.PASS;
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+      user: user,
+      pass: pass
+  }
+});
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login", { "message": req.flash("error") });
@@ -26,6 +37,9 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
+  const confirmationCode = req.body.confirmationCode
+  console.log("hola")
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -42,17 +56,32 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      email,
+      confirmationCode
     });
 
     newUser.save()
     .then(() => {
-      res.redirect("/");
+      transporter.sendMail({
+        from: email, // sender address
+        to: 'martaguirre91@gmail.com',
+        subject: "Hello ✔", // Subject line
+        html: `<h1>Hello its working! Hi ${newUser.username}</h1>
+        http://localhost:3000/auth/confirm/${newUser.confirmationCode}`
+    })
+    .then(info => {
+      console.log(info)
+      res.redirect("/")
+    })
+    .catch(error => console.log(error))
     })
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong" });
     })
-  });
+  })
+
+
 });
 
 router.get("/logout", (req, res) => {
