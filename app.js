f6b3c5ff3b4875751b56a1b06aab36531c84c6ab
Jose Luis Rodriguez <j_cerra@hotmail.com>
2021-02-15 17:24:28 +0100

    done

---
+++ app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const index = require('./routes/index.routes');
+const User = require("./models/user.model")
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// require database configuration
+require('./configs/db.config');
+const session = require('./configs/session.config');
+app.use(session);
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
+app.use((req, res, next) => {
+    if (req.session.currentUserId) {
+      User.findById(req.session.currentUserId)
+        .then(user => {
+          if (user) {
+            req.currentUser = user
+            res.locals.currentUser = user
+  
+            next()
+          }
+        })
+    } else {
+      next()
+    }
+  })
+
+app.use('/', index);
+
+module.exports = app;
