5b3ab5f557e04a0ec2c52b08e49f622e8dd49877
Marina Laporte Boni <marinaolboni@gmail.com>
2022-08-22 18:02:09 +0200

    lab done

---
+++ controllers/auth.controller.js
@@ -0,0 +1,92 @@
+const User = require("../models/User.model");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const mailer = require("../config/mailer.config");
+
+module.exports.register = (req, res, next) => {
+  res.render("auth/register");
+};
+
+module.exports.doRegister = (req, res, next) => {
+  const user = req.body;
+
+  const renderWithErrors = (errors) => {
+    res.render("auth/register", { errors, user });
+  };
+
+  User.findOne({ email: user.email })
+    .then((userFound) => {
+      if (userFound) {
+        renderWithErrors("Email already exist");
+      } else {
+        return User.create(user).then((userCreated) => {
+          mailer.sendActivationMail(
+            userCreated.email,
+            userCreated.activationToken
+          );
+          res.redirect("/login");
+        });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(err.errors);
+      } else {
+        next(err);
+      }
+    });
+};
+
+const login = (req, res, next, provider) => {
+  passport.authenticate(provider || "local-auth", (err, user, validations) => {
+    if (err) {
+      next(err);
+    } else if (!user) {
+      res.status(404).render("auth/login", { errors: validations.error });
+    } else {
+      req.login(user, (loginError) => {
+        if (loginError) {
+          next(loginError);
+        } else {
+          res.redirect("/profile");
+        }
+      });
+    }
+  })(req, res, next);
+};
+
+module.exports.login = (req, res, next) => {
+  res.render("auth/login");
+};
+
+module.exports.doLogin = (req, res, next) => {
+  login(req, res, next);
+};
+
+module.exports.doLoginGoogle = (req, res, next) => {
+  login(req, res, next, "google-auth");
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logout(() => res.redirect("/login"));
+};
+
+module.exports.activateAccount = (req, res, next) => {
+  const token = req.params.token;
+
+  User.findOneAndUpdate(
+    { activationToken: token, active: false },
+    { active: true }
+  )
+    .then((user) => {
+      if (user) {
+        res.render("auth/login", {
+          user: { email: user.email },
+          message: "You have activated your account. Thanks for joining!",
+        });
+      } else {
+        res.redirect("/login");
+      }
+    })
+    .catch(next);
+};
